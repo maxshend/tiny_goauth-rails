325611bd9eb44e27a52c4aaa05c9ad395fc32ba2
maxshend <shendrik.mv@gmail.com>
2020-10-28 02:13:22 +0300

    Change field name of the user ID. Add email to the migration templates.

--- app/interactions/tiny_goauth/create_user.rb
+++ app/interactions/tiny_goauth/create_user.rb
@@ -2,15 +2,15 @@
 
 module TinyGoauth
   class CreateUser < ::ActiveInteraction::Base
-    integer :auth_user_id
+    integer :id
     string :email
 
     def execute
-      @user = User.find_by auth_user_id: auth_user_id
+      @user = User.find_by auth_user_id: id
 
       return if @user
 
-      @user = User.new email: email, auth_user_id: auth_user_id
+      @user = User.new email: email, auth_user_id: id
       errors.merge! @user unless @user.save
 
       @user
